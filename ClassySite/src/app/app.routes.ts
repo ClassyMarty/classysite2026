c61517ba59a9a269bad9bd3f3341565d8c7ff397
ClassyMarty <68179598+ClassyMarty@users.noreply.github.com>
2026-02-11 11:15:04 +0200

    some tweaks and getting the menus to work

--- ClassySite/src/app/app.routes.ts
+++ ClassySite/src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Resume } from './pages/resume/resume';
+import { Hobbies } from './pages/hobbies/hobbies';
+import { ThisSite } from './pages/this-site/this-site';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
     { path: '', component: Home },
+    { path: 'resume', component: Resume },
+    { path: 'hobbies', component: Hobbies },
+    { path: 'thissite', component: ThisSite },
+    { path: 'contact', component: Contact },
 ];
---
+++ ClassySite/src/app/pages/this-site/this-site.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-this-site',
+  imports: [],
+  templateUrl: './this-site.html',
+  styleUrl: './this-site.scss',
+})
+export class ThisSite {
+
+}
