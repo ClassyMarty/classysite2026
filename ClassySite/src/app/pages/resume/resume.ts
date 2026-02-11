545cfe35cd67f547fcf7704eedea6dc2edb93e3b
ClassyMarty <68179598+ClassyMarty@users.noreply.github.com>
2026-02-11 16:09:29 +0200

    resume start

--- ClassySite/src/app/pages/resume/resume.ts
+++ ClassySite/src/app/pages/resume/resume.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-resume',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIcon,
+    MatListModule
+
+  ],
   templateUrl: './resume.html',
   styleUrl: './resume.scss',
 })
