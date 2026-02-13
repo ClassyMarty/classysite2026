5113bfe89b114ce5b5080393a54d5a0fa88a07ae
ClassyMarty <68179598+ClassyMarty@users.noreply.github.com>
2026-02-13 06:17:34 +0200

    cssgen

---
+++ ClassySite/src/app/components/cssgen/cssgen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cssgen',
+  imports: [],
+  templateUrl: './cssgen.html',
+  styleUrl: './cssgen.scss',
+})
+export class Cssgen {
+
+}
